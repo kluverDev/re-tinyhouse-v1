73a03fd8a86f476462231fae88d71a1b1c8aa63f
wizzy <kluverdev@gmail.com>
2021-10-14 08:00:54 -0400

    creating a minimal node express server

--- server/index.js
+++
@@ -1,4 +0,0 @@
-console.log("hello world");
-const one = 1;
-const two = 2;
-console.log(`${one + two}`);
---
+++ server/src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+const port = 9000;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+console.log(`[app] : http://localhost:${port}`);
+
+app.listen(port);

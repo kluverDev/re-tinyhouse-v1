e032584bb9fe9aaff8931eef2ee96a011fd61f1a
wizzy <kluverdev@gmail.com>
2021-10-14 11:15:21 -0400

    linting with eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,20 @@
 import express from "express";
 
 const app = express();
-
+const num = 1;
 const port = 9000;
-app.get("/", (req, res) => {
+app.get("/", (_req, res) => {
   res.send("hello world");
 });
+const three = false;
+
+// const three: string = "one";
+
+// const three: null = null;
+
+// const three: undefined = undefined;
+
+// const three: any = {};
 
 console.log(`[app] : http://localhost:${port}`);
 

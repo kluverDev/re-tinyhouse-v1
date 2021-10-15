9849e3b88a7bad2e2c607b4168e800fda3271eb9
wizzy <kluverdev@gmail.com>
2021-10-15 07:03:57 -0400

    creating a get and post express route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,24 @@
 import express from "express";
 
+import { listings } from "./listings";
+
 const app = express();
-const num = 1;
+app.use(express.json());
+
 const port = 9000;
-app.get("/", (_req, res) => {
-  res.send("hello world");
+app.get("/listings", (_req, res) => {
+  res.send(listings);
 });
-const three = false;
+app.post("/delete-listing", (req, res) => {
+  const id: string = req.body.id;
+
+  for (let i = 0; i < listings.length; i++) {
+    if (listings[i].id === id) {
+      return res.send(listings.splice(i, 1));
+    }
+    return res.send("failed to find listings");
+  }
+});
 
 // const three: string = "one";
 

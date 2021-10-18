000e86f52ca672fadadb1374384f75024c02e1d3
wizzy <kluverdev@gmail.com>
2021-10-18 08:40:03 -0400

    using graphql schema language

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,33 +1,15 @@
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
-
-import { listings } from "./listings";
+//import { makeExecutableSchema } from "graphql-tools";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
-app.use(express.json());
-
 const port = 9000;
-app.get("/listings", (_req, res) => {
-  res.send(listings);
-});
-app.post("/delete-listing", (req, res) => {
-  const id: string = req.body.id;
 
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1));
-    }
-    return res.send("failed to find listings");
-  }
-});
+//const schema = makeExecutableSchema({ typeDefs, resolvers });
+const server = new ApolloServer({ typeDefs, resolvers });
 
-// const three: string = "one";
-
-// const three: null = null;
-
-// const three: undefined = undefined;
-
-// const three: any = {};
-
-console.log(`[app] : http://localhost:${port}`);
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
+console.log(`[app] : http://localhost:${port}`);

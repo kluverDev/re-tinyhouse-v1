0a19a574cdeefc14acc23d0fe353633979d9853d
wizzy <kluverdev@gmail.com>
2022-01-07 18:32:11 -0400

    typescript generics and adding types to our collections

--- server/src/database/index.ts
+++ server/src/database/index.ts
@@ -1,10 +1,12 @@
 import { MongoClient } from "mongodb";
+import { Database } from "../lib/types";
 
 const user = "user1";
-
+const password = "dYLvACcOtJwA1bG8";
+const cluster = "cluster0.6dtif";
 const url = `mongodb+srv://${user}:${password}@${cluster}.mongodb.net/main?retryWrites=true&w=majority`;
 
-export const connectDatabase = async () => {
+export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,

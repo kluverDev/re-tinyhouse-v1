953e2f4634e91fca18eb7a276883153321f15e46
wizzy <kluverdev@gmail.com>
2022-01-22 20:19:48 -0400

    querying listings

--- client/src/sections/Listings/Listings.tsx
+++ client/src/sections/Listings/Listings.tsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { server } from "../../lib/api";
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBeds
+      numOfBaths
+      rating
+    }
+  }
+`;
 
 interface Props {
   title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-  return <h2>{title}</h2>;
+  const fetchListings = async () => {
+    const { data } = await server.fetch({ query: LISTINGS });
+    console.log(data, "my baby");
+  };
+  return (
+    <div>
+      <h2>{title}</h2>
+      <button onClick={fetchListings}>Query ;listings</button>
+    </div>
+  );
 };
